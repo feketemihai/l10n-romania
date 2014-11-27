237de7d48f2be2ee18bdbef1dc44f988fe3a137f
Dorin Hongu <dhongu@gmail.com>
2014-11-27 22:59:20 +0200

    Adaugare raport factura

---
+++ l10n_ro_invoice_report/account_invoice.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2008 Deltatech All Rights Reserved
+#                    Dorin Hongu <dhongu(@)gmail(.)com       
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+##############################################################################
+
+
+from openerp import models, fields, api, _
+import openerp.addons.decimal_precision as dp
+
+
+class account_invoice_line(models.Model):
+    _inherit = "account.invoice.line"
+    
+    @api.one
+    @api.depends('price_unit', 'discount', 'invoice_line_tax_id', 'quantity',
+        'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id')
+    def _compute_price(self):
+        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
+        taxes = self.invoice_line_tax_id.compute_all(price, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
+        self.price_subtotal = taxes['total']
+        self.price_taxes = taxes['total_included']-taxes['total']
+        if self.invoice_id:
+            self.price_subtotal = self.invoice_id.currency_id.round(self.price_subtotal)    
+            self.price_taxes = self.invoice_id.currency_id.round(self.price_taxes)    
+    
+    price_taxes = fields.Float(string='Taxes', digits= dp.get_precision('Account'),
+        store=True, readonly=True, compute='_compute_price')
