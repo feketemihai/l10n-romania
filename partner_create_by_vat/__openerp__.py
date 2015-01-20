3f5fbe721d032568c9393b81ec12cc2cc75fde72
Adrian-Ioan Vasile <yoyo@opennet.ro>
2015-01-20 13:20:16 +0200

    updated module description

--- partner_create_by_vat/__openerp__.py
+++ partner_create_by_vat/__openerp__.py
@@ -26,9 +26,9 @@
     "category" : "Hidden",
     "depends": ['l10n_ro','base_vat'],
     "description": """
-Partner create by vat
+Partner create by VAT
 ---------------------
-Fetches Partner data from openapi/mfinante of VIES
+Fetches Partner data from openapi/mfinante or VIES
     """,
 
     'data': ['res_partner_view.xml',
