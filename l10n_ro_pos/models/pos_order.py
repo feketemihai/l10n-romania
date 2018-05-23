9d9ebd5d7715c211d4d7ad3f85f1ad09cb6b6d75
Dorin Hongu <dhongu@gmail.com>
2018-05-23 08:04:54 +0300

    determinare jurnal numerar la plata rambursare din pos

--- l10n_ro_pos/models/pos_order.py
+++ l10n_ro_pos/models/pos_order.py
@@ -3,12 +3,35 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from odoo import api, models, _
+from odoo import api, models, fields, _
 
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
+
+
+    # fix bug
+    @api.multi
+    def button_dummy(self):
+        return True
+
+
     @api.multi
     def action_pos_order_invoice(self):
         return super(PosOrder, self.with_context(from_pos_order=True)).action_pos_order_invoice()
+
+class PosOrderLine(models.Model):
+    _inherit = "pos.order.line"
+
+    tax_ids = fields.Many2many('account.tax', string='Taxes', readonly=False)
+
+    #fix bug
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        if self.product_id:
+            tax_ids = self.product_id.taxes_id.filtered( lambda r: not self.company_id or r.company_id == self.company_id)
+            fpos = self.order_id.fiscal_position_id
+            self.tax_ids_after_fiscal_position = fpos.map_tax(tax_ids, self.product_id, self.order_id.partner_id) if fpos else tax_ids
+        return super(PosOrderLine, self)._onchange_product_id()
+
--- l10n_ro_pos/wizard/__init__.py
+++ l10n_ro_pos/wizard/__init__.py
@@ -3,11 +3,4 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from . import models
-from . import report
-from . import wizard
-
-
-
-
-
+from . import pos_payment
