642ef9928cc854cb36f1831f9c50feb0fa716d0b
Dorin Hongu <dhongu@gmail.com>
2018-08-27 14:58:53 +0300

    fix

--- l10n_ro_stock_picking_report/models/account_invoice.py
+++ l10n_ro_stock_picking_report/models/account_invoice.py
@@ -34,9 +34,10 @@ class AccountInvoice(models.Model):
 
     def _prepare_invoice_line_from_po_line(self, line):
         data = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
-        if self.reference:
-            self.reference += self.purchase_id.partner_ref
-        else:
-            self.reference = self.purchase_id.partner_ref
+        if self.purchase_id.partner_ref:
+            if self.reference:
+                self.reference += self.purchase_id.partner_ref
+            else:
+                self.reference = self.purchase_id.partner_ref
 
         return data
