6b0d8e43d543a6bb806282421485cbc9b96368a2
Dorin Hongu <dhongu@gmail.com>
2018-09-11 21:27:32 +0300

    registrul de casa

---
+++ l10n_ro_account_report/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Romania - Account Reports",
+    "version": "1.0",
+    "author": "Dorin Hongu",
+    "website": "",
+
+    "category": "Romania Adaptation",
+    "depends": [
+        'account',
+    ],
+
+    "description": """
+Romania  - Accounting Reports
+------------------------------------------
+
+
+    """,
+
+    'data': [
+        'views/account_statement_report.xml',
+        'views/report_statement.xml'
+    ],
+
+    'installable': True,
+    'auto_install': False,
+}
