e85c0d4e00d6ab08871ac0de0cdf9639619cc14e
Dorin Hongu <dhongu@gmail.com>
2018-09-14 08:04:34 +0300

    restrictie la tiparire documente de stoc

--- l10n_ro_stock_picking_report/models/stock.py
+++ l10n_ro_stock_picking_report/models/stock.py
@@ -89,8 +89,11 @@ class stock_picking(models.Model):
     def do_print_picking(self):
         self.write({'printed': True})
         if self.picking_type_code == 'incoming':
-            # res = self.env['report'].get_action(self, 'l10n_ro_stock_picking_report.report_reception')
-            res= self.env.ref('l10n_ro_stock_picking_report.action_report_reception').report_action(self)
+            if self.location_dest_id.merchandise_type == 'store':
+                res = self.env.ref('l10n_ro_stock_picking_report.action_report_reception').report_action(self)
+            else:
+                # res = self.env['report'].get_action(self, 'l10n_ro_stock_picking_report.report_reception')
+                res= self.env.ref('l10n_ro_stock_picking_report.report_reception_sale_price').report_action(self)
         elif self.picking_type_code == 'outgoing':
             # res = self.env['report'].get_action(self, 'l10n_ro_stock_picking_report.report_delivery')
             res = self.env.ref('l10n_ro_stock_picking_report.action_report_delivery').report_action(self)
