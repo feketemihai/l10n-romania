b98a343c7208df977886fef3369f99d9b135cb07
Dorin Hongu <dhongu@gmail.com>
2020-08-07 08:22:31 +0300

    Calcul pret, eliminare _formatLang

--- l10n_ro_stock_picking_report/report/picking.py
+++ l10n_ro_stock_picking_report/report/picking.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# ©  2008-2019 Deltatech
+# ©  2008-20209 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
@@ -7,7 +7,7 @@
 import time
 
 from odoo import api, models
-from odoo.tools import formatLang
+
 from functools import reduce
 
 
@@ -25,16 +25,15 @@ class ReportPickingDelivery(models.AbstractModel):
             'data': data,
             'time': time,
             'docs': self.env[report.model].browse(docids),
-            'formatLang': self._formatLang,
+
             'get_line': self._get_line,
             'get_totals': self._get_totals,
             'reduce': reduce
         }
 
-    def _formatLang(self, value, **kwargs):
-        if 'date' in kwargs:
-            return value
-        return formatLang(self.env, value, **kwargs)
+
+
+
 
     def _get_line(self, move_line):
         res = {'price': 0.0, 'amount': 0.0, 'tax': 0.0, 'amount_tax': 0.0}
@@ -92,48 +91,51 @@ class ReportPickingReception(models.AbstractModel):
             'data': data,
             'time': time,
             'docs': self.env[report.model].browse(docids),
-            'formatLang': self._formatLang,
+
             'get_line': self._get_line,
             'get_totals': self._get_totals,
             'reduce': reduce
         }
 
-    def _formatLang(self, value, **kwargs):
-        # todo: de tratat : formatLang(totals['amount'], currency_obj=res_company.currency_id)
-        # todo: de tratat : formatLang(o.date, date=True)
-        if 'date' in kwargs:
-            return value
-        return formatLang(self.env, value, **kwargs)
 
-    def _get_line(self, move_line):
+
+    def _get_line(self, move):
         res = {'price': 0.0, 'amount': 0.0, 'tax': 0.0,
                'amount_tax': 0.0, 'amount_sale': 0.0, 'margin': 0.0}
 
-        currency = move_line.company_id.currency_id
+        currency = move.company_id.currency_id
 
-        if move_line.purchase_line_id:
-            # todo: ce fac cu receptii facute ca preturi diferite ????
-            line = move_line.purchase_line_id
+        if move.purchase_line_id:
+            # todo: ce fac cu receptii facute cu preturi diferite ????
+            line = move.purchase_line_id
 
             # todo: de verificat daca pretul din miscare este actualizat inainte de confirmarea transferului pentru a se actualiza cursul valutar !!
-            res['price'] = move_line.price_unit  # pretul caculat la genereare miscarii
+            res['price'] = move.price_unit  # pretul caculat la genereare miscarii
+
+            value = 0
+            quantity = 0
+            for valuation in move.stock_valuation_layer_ids:
+                value += valuation.value
+                quantity += valuation.quantity
+            if move.stock_valuation_layer_ids:
+                res['price'] = value / (quantity or 1)
 
             # la loturi nu este completat move_line.price_unit
-            if move_line.price_unit == 0:
-                if move_line.remaining_qty != 0:
-                    res['price'] = move_line.remaining_value /  move_line.remaining_qty
+            # if move_line.price_unit == 0:
+            #     if move_line.remaining_qty != 0:
+            #         res['price'] = move_line.remaining_value /  move_line.remaining_qty
 
             taxes = line.taxes_id.compute_all(res['price'],
-                                              quantity=move_line.product_qty,
-                                              product=move_line.product_id,
-                                              partner=move_line.partner_id)
+                                              quantity=move.product_qty,
+                                              product=move.product_id,
+                                              partner=move.partner_id)
 
             res['tax'] = taxes['total_included'] - taxes['total_excluded']
             res['amount'] = taxes['total_excluded']
             res['amount_tax'] = taxes['total_included']
 
-            taxes_ids = line.product_id.taxes_id.filtered(lambda r: r.company_id == move_line.company_id)
-            list_price = move_line.product_id.list_price
+            taxes_ids = line.product_id.taxes_id.filtered(lambda r: r.company_id == move.company_id)
+            list_price = move.product_id.list_price
             # incl_tax = taxes_ids.filtered(lambda tax: tax.price_include)
             # if incl_tax:
             #     list_price = incl_tax.compute_all(move_line.product_id.list_price)['total_excluded']
@@ -141,12 +143,13 @@ class ReportPickingReception(models.AbstractModel):
             #     list_price = move_line.product_id.list_price
 
             taxes_sale = taxes_ids.compute_all(list_price, currency=currency,
-                                               quantity=move_line.product_uom_qty,
-                                               product=move_line.product_id)
+                                               quantity=move.product_uom_qty,
+                                               product=move.product_id)
 
             res['amount_sale'] = taxes_sale['total_excluded']
             res['tax_sale'] = taxes_sale['total_included'] - taxes_sale['total_excluded']
             res['amount_tax_sale'] = taxes_sale['total_included']
+            #  conversie pret din pretul din unitatea de masura de baza in pret in unitatea de masura din document
             res['price'] = res['price'] * line.product_uom._compute_quantity(1, line.product_id.uom_id)
             if res['amount_tax'] != 0.0:
                 res['margin'] = 100 * (taxes_sale['total_included'] - res['amount_tax']) / res['amount_tax']
@@ -155,8 +158,8 @@ class ReportPickingReception(models.AbstractModel):
         else:
             # receptie fara comanda de aprovizionare
 
-            #value = move_line.value
-            res['price'] = abs(move_line.price_unit)
+            value = move.value
+            res['price'] = abs(move.price_unit)
 
             # res['amount'] = currency.round(value)
             # if move_line.product_uom_qty != 0:
@@ -164,26 +167,26 @@ class ReportPickingReception(models.AbstractModel):
             # else:
             #     res['price'] = 0.0
 
-            taxes_ids = move_line.product_id.supplier_taxes_id.filtered(lambda r: r.company_id == move_line.company_id)
+            taxes_ids = move.product_id.supplier_taxes_id.filtered(lambda r: r.company_id == move.company_id)
             taxes = taxes_ids.compute_all(res['price'], currency=currency,
-                                          quantity=move_line.product_uom_qty,
-                                          product=move_line.product_id,
-                                          partner=move_line.partner_id)
+                                          quantity=move.product_uom_qty,
+                                          product=move.product_id,
+                                          partner=move.partner_id)
             res['amount'] = taxes['total_excluded']
             res['tax'] = taxes['total_included'] - taxes['total_excluded']
             res['amount_tax'] = taxes['total_included']
 
-            taxes_ids = move_line.product_id.taxes_id.filtered(lambda r: r.company_id == move_line.company_id)
+            taxes_ids = move.product_id.taxes_id.filtered(lambda r: r.company_id == move.company_id)
             incl_tax = taxes_ids.filtered(lambda tax: tax.price_include)
             # if incl_tax:
             #     list_price = incl_tax.compute_all(move_line.product_id.list_price)['total_excluded']
             # else:
 
-            list_price = move_line.product_id.list_price
+            list_price = move.product_id.list_price
 
             taxes_sale = taxes_ids.compute_all(list_price, currency=currency,
-                                               quantity=move_line.product_uom_qty,
-                                               product=move_line.product_id)
+                                               quantity=move.product_uom_qty,
+                                               product=move.product_id)
 
             res['amount_sale'] = taxes_sale['total_excluded']
             res['tax_sale'] = taxes_sale['total_included'] - taxes_sale['total_excluded']
@@ -213,6 +216,7 @@ class ReportPickingReception(models.AbstractModel):
 
 class report_delivery(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_delivery'
+    _description = 'Report delivery'
     _inherit = 'report.abstract_report.delivery_report'
     _template = 'l10n_ro_stock_picking_report.report_delivery'
     # _wrapped_report_class = picking_delivery
@@ -220,6 +224,7 @@ class report_delivery(models.AbstractModel):
 
 class report_delivery_price(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_delivery_price'
+    _description = 'Report delivery in store'
     _inherit = 'report.abstract_report.delivery_report'
     _template = 'l10n_ro_stock_picking_report.report_delivery_price'
     # _wrapped_report_class = picking_delivery
@@ -227,6 +232,7 @@ class report_delivery_price(models.AbstractModel):
 
 class report_consume_voucher(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_consume_voucher'
+    _description = 'Report consume voucher'
     _inherit = 'report.abstract_report.delivery_report'
     _template = 'l10n_ro_stock_picking_report.report_consume_voucher'
     # _wrapped_report_class = picking_delivery
@@ -234,6 +240,7 @@ class report_consume_voucher(models.AbstractModel):
 
 class report_internal_transfer(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_internal_transfer'
+    _description = 'Report transfer'
     _inherit = 'report.abstract_report.delivery_report'
     _template = 'l10n_ro_stock_picking_report.report_internal_transfer'
     # _wrapped_report_class = picking_delivery
@@ -241,6 +248,7 @@ class report_internal_transfer(models.AbstractModel):
 
 class report_reception(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_reception'
+    _description = 'Report reception'
     _inherit = 'report.abstract_report.reception_report'
     _template = 'l10n_ro_stock_picking_report.report_reception'
     # _wrapped_report_class = picking_reception
@@ -248,6 +256,7 @@ class report_reception(models.AbstractModel):
 
 class report_reception_no_tax(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_reception_no_tax'
+    _description = 'Report reception no tax'
     _inherit = 'report.abstract_report.reception_report'
     _template = 'l10n_ro_stock_picking_report.report_reception_no_tax'
     # _wrapped_report_class = picking_reception
@@ -255,6 +264,7 @@ class report_reception_no_tax(models.AbstractModel):
 
 class report_reception_sale_price(models.AbstractModel):
     _name = 'report.l10n_ro_stock_picking_report.report_reception_sale_price'
+    _description = 'Report reception in store'
     _inherit = 'report.abstract_report.reception_report'
     _template = 'l10n_ro_stock_picking_report.report_reception_sale_price'
     # _wrapped_report_class = picking_reception
