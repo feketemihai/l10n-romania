c051ae369169bd5e41395443980fea8b8278a006
Dorin Hongu <dhongu@gmail.com>
2020-08-29 09:31:42 +0300

    mutare teste in legate de contabilitatea stocurilor din magazin

---
+++ l10n_ro_stock_account_store/__manifest__.py
@@ -0,0 +1,24 @@
+# ©  2008-2020 Fekete Mihai <mihai.fekete@forbiom.eu>
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Romania - Stock Accounting Store",
+    "version": "13.0.1.0.0.",
+    "category": "Localization",
+    "summary": "Romania - stock account",
+    "author": "NextERP Romania,"
+              "Dorin Hongu,"
+              "Forest and Biomass Romania,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-romania",
+    "depends": [
+        "l10n_ro_stock_account",
+
+    ],
+    "license": "AGPL-3",
+    "data": [
+
+    ],
+    "installable": True,
+    "maintainers": ["dhongu"],
+}
