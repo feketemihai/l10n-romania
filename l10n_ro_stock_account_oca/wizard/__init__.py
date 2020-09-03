e1a33ddb8a844d29412d211127e4a04b92d44a1b
Dorin Hongu <dhongu@gmail.com>
2020-09-03 08:32:18 +0300

    eliminare stoc_account_oca

--- l10n_ro_stock_account_oca/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# ©  2008-2018 Fekete Mihai <mihai.fekete@forbiom.eu>
-#              Dorin Hongu <dhongu(@)gmail(.)com
-# See README.rst file on addons root folder for license details
-
-import stock_invoice_onshipping
-from . import stock_change_standard_price
